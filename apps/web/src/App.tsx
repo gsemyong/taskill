b73be8d4d6ebfc17dcbd4ec30bac2ac7aabd6842
gsemy <gsemyong@gmail.com>
2023-09-25 12:56:22 +0200

    Add trpc

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,30 @@
-function App() {
-  return <div className="text-5xl font-black">hello</div>;
-}
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { useState } from "react";
+import { trpc } from "./lib/trpc";
 
-export default App;
+export function App() {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:2022/trpc",
+
+          // You can pass any HTTP headers you wish here
+          async headers() {
+            return {};
+          },
+        }),
+      ],
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {/* Your app here */}
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
